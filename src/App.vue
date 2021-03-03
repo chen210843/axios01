4d50eafe30c4d1c1fc82c15f1ea4409f5929fb7b
chen210843 <siri.wut210843@gmail.com>
2021-03-03 16:09:47 +0700

    app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <div class="sm-12 md1-2">
+   <b-navbar  type="dark" variant="dark">
+    <b-navbar-brand href="#"> <router-link to="/">photos</router-link></b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+ <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        </b-nav-form>
+      </b-navbar-nav>
+  </b-navbar>
+</div>
+      
     </div>
     <router-view />
   </div>
